7f9327b16fc96646fb7a57dc38258f73b9fd3f32
ecohnoch <542305306@qq.com>
2021-09-24 22:05:06 +0800

    architecture optimization

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vsource',
-    version='v1.1.2',
+    version='v1.1.3',
     description=(
         'Algorithm python library linked to vsource platform.'
     ),
--- vsource/__init__.py
+++ vsource/__init__.py
@@ -18,10 +18,12 @@ from vsource.algorithms.salient_object_detection import salient_object_detection
 from vsource.algorithms.landmark_detection import landmark_detection
 from vsource.algorithms.binary_segmentation import binary_segmentation
 from vsource.algorithms.face_attribute_x import face_attribute_x
+from vsource.algorithms.text_generation import text_generation
+from vsource.algorithms.github_avatar_generation import github_avatar_generation
 
 from vsource.scripts.algorithm_scripts.algorithms import parse_algorithm
 from vsource.scripts.enroll_service import enroll
 
-__version__ = 'v1.1.2'
+__version__ = 'v1.1.3'
 __author__  = 'Ecohnoch'
 __email__   = 'chuyuan@vsource.club'
