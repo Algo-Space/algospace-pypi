bedda9357d0051aed6ae1ea3ba72df664199475b
Kermit <497950719@qq.com>
2022-12-06 21:25:13 +0800

    release: v0.1.3

--- algospace/__init__.py
+++ algospace/__init__.py
@@ -2,7 +2,7 @@
 AlgoSpace: A platform for displaying algorithm achievements
 '''
 
-__version__ = 'v0.1.2'
+__version__ = 'v0.1.3'
 
 from algospace.__main__ import run
 from algospace.customer.service import login
